078345c44a876f7c7401218ed21e35880ab78733
lance.high <lance.high@hastings.edu>
2021-12-14 15:52:07 -0600

    HTTPResponse Send complete

--- HTTPLib/HTTPResponse.cs
+++ HTTPLib/HTTPResponse.cs
@@ -11,28 +11,6 @@ namespace HTTPLib {
         // Check this, but if a 
         private Stream sBody = Stream.Null;
 
-        public string Status
-        {
-            get
-            {
-                return strStatusLine;
-            }
-        }
-        public List<string> Headers
-        {
-            get
-            {
-                return lstHeaders;
-            }
-        }
-        public Stream Body
-        {
-            get
-            {
-                return sBody;
-            }
-        }
-
         public HTTPResponse(string strStatus, List<string> Headers, Stream Body)
         {
             strStatusLine = strStatus;
@@ -43,9 +21,23 @@ namespace HTTPLib {
         {
             // NEED TO: Convert the response into bytes (in blocks of 1024 bytes)
             // and write it to strmOut
-
-            
-
+            string strStatus = strStatusLine + "\r\n";
+            byte[] byStatus = Encoding.ASCII.GetBytes(strStatus);
+            string strHeaders = "";
+            foreach (string strH in lstHeaders)
+            {
+                strHeaders += strH + "\r\n";
+            }
+            strHeaders += "\r\n";
+            byte[] byHeaders = Encoding.ASCII.GetBytes(strHeaders);
+            // Send status, then headers, then body
+            strmOut.Write(byStatus, 0, byStatus.Length);
+            strmOut.Write(byHeaders, 0, byHeaders.Length);
+            byte[] byBody = new byte[1024];
+            int iBytesRead;
+            while ((iBytesRead = sBody.Read(byBody, 0, 1024)) > 0){
+                strmOut.Write(byBody, 0, iBytesRead);
+            }
         }
     }
 }
